a169ba0cf09c3eeaddf9ae0e71298c41607b80e1
Jacob Vosmaer <contact@jacobvosmaer.nl>
2023-11-10 20:25:41 +0100

    Hard-code algo and frequency

--- DaisyX7.cpp
+++ DaisyX7.cpp
@@ -2,19 +2,24 @@
 #include "daisy_pod.h"
 #include <string.h>
 
+#define nelem(x) (sizeof(x) / sizeof(*x))
+
 using namespace daisy;
 
 DaisyPod hw;
 
 struct {
-  struct {
-    float phase;
-  } osc[NUM_OPS];
+  float phase[NUM_OPS];
   float previous_sample;
   float feedback[2];
   float mem;
   int algo;
+  int feedbacklevel;
 } ops;
+
+struct {
+  float freqhz, amp;
+} egs[NUM_OPS];
 
 static void AudioCallback(AudioHandle::InterleavingInputBuffer in,
                           AudioHandle::InterleavingOutputBuffer out,
@@ -25,6 +30,13 @@ static void AudioCallback(AudioHandle::InterleavingInputBuffer in,
 
 int main(void) {
   hw.Init();
+
+  ops.algo = 1;        /* Algorithm 2 */
+  egs[5].freqhz = 220; /* Operator 1: 220Hz, full volume */
+  egs[5].amp = 1;
+  egs[4].freqhz = 220; /* Operator 2: 220Hz, half volume */
+  egs[4].amp = 0.5;
+
   hw.StartAudio(AudioCallback);
 
   while (1)
