3afde18ee5272b8b86417e880f81b4d558183324
Jacob Vosmaer <contact@jacobvosmaer.nl>
2023-11-10 22:02:57 +0100

    Should make sound but does not

--- DaisyX7.cpp
+++ DaisyX7.cpp
@@ -1,8 +1,10 @@
 #include "algo.h"
 #include "daisy_pod.h"
+#include <math.h>
 #include <string.h>
 
-#define nelem(x) (sizeof(x) / sizeof(*x))
+#define nelem(x) (int)(sizeof(x) / sizeof(*x))
+float pi = 3.141592653;
 
 using namespace daisy;
 
@@ -10,7 +12,7 @@ DaisyPod hw;
 
 struct {
   float phase[NUM_OPS];
-  float previous_sample;
+  float mod;
   float feedback[2];
   float mem;
   int algo;
@@ -21,21 +23,83 @@ struct {
   float freqhz, amp;
 } egs[NUM_OPS];
 
+float egs_freq(int op) {
+  if (op < 0 || op >= nelem(egs))
+    return 0;
+
+  return egs[op].freqhz / hw.AudioSampleRate();
+}
+
+float ops_feedback(void) {
+  /* TODO return scale factor based on ops.feedback_level */
+  return 0;
+}
+
+float ops_update(int i) {
+  struct algorithm algo = algorithms[ops.algo][i],
+                   previous_algo =
+                       algorithms[ops.algo][(i + 1) % nelem(algorithms[0])];
+
+  /* Calculate sample for current operator i based on ops.mod from previous
+   * operator */
+  float sample = sinf(pi * (ops.phase[i] + ops.mod)) * egs[i].amp /
+                 ((float)1 + previous_algo.com);
+
+  /* Calculate ops.mod and ops.mem for use in next call to ops_update() */
+
+  if (algo.a) {
+    ops.feedback[1] = ops.feedback[0];
+    ops.feedback[0] = sample;
+  }
+
+  float mem = (float)algo.c * ops.mem + (float)algo.d * sample;
+
+  switch (algo.sel) {
+  case 0:
+    ops.mod = 0;
+    break;
+  case 1:
+    ops.mod = sample;
+    break;
+  case 2:
+    ops.mod = mem;
+    break;
+  case 3:
+    ops.mod = ops.mem;
+    break;
+  case 4:
+    ops.mod = ops.feedback[0];
+    break;
+  case 5:
+    ops.mod = ops_feedback() * (ops.feedback[0] + ops.feedback[1]) / 2.0;
+    break;
+  }
+  ops.mem = mem;
+
+  return ops.mem;
+}
+
 static void AudioCallback(AudioHandle::InterleavingInputBuffer in,
                           AudioHandle::InterleavingOutputBuffer out,
                           size_t size) {
-  (void)ops;
-  memset(out, 0, size * sizeof(*out));
+  for (int j = 0; j < (int)size; j += 2) {
+    float sample;
+    for (int i = nelem(egs); i--;) {
+      ops.phase[i] += egs_freq(i);
+      if (ops.phase[i] > 1)
+        ops.phase[i] -= 2;
+      sample = ops_update(i);
+    }
+    out[j] = out[j + 1] = sample;
+  }
 }
 
 int main(void) {
   hw.Init();
 
-  ops.algo = 1;        /* Algorithm 2 */
-  egs[5].freqhz = 220; /* Operator 1: 220Hz, full volume */
+  ops.algo = 0;        /* Algorithm 2 */
+  egs[5].freqhz = 110; /* Operator 1: 220Hz, full volume */
   egs[5].amp = 1;
-  egs[4].freqhz = 220; /* Operator 2: 220Hz, half volume */
-  egs[4].amp = 0.5;
 
   hw.StartAudio(AudioCallback);
 
